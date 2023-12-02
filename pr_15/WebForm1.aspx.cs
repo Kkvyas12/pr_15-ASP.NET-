852b5948a659d2a675f5127ff6dc728c83072982
vyas <vyas@DESKTOP-6QA1C6R>
2023-12-02 06:31:01 -0800

    Add project files.

---
+++ pr_15/WebForm1.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace pr_15
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\vyas\source\repos\pr_15\pr_15\App_Data\Database1.mdf;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("select * from [dbo].[user] where user='"+TextBox1.Text+"' and pass='"+TextBox2.Text+"';", con);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable user = new DataTable();
+            sda.Fill(user);
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+            if (user.Rows.Count > 0)
+            {
+                Session["id"] = TextBox1.Text;
+                Response.Redirect("Redirectform.aspx");
+                Session.RemoveAll();
+            }
+            else
+            {
+                Label1.Text = "You're username and word is incorrect";
+                Label1.ForeColor = System.Drawing.Color.Red;
+
+            }
+        }
+    }
+}
---
+++ pr_15/redirectform.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace pr_15
+{
+    public partial class redirectform : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label1.Text = Session["id"].ToString();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Session.RemoveAll();
+            Response.Redirect("Default.aspx");
+        }
+    }
+}
